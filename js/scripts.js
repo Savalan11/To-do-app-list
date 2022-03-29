9db408126e96633cce88e74880d9f38109449458
Savalan Suleymanli <98057609+Savalan11@users.noreply.github.com>
2022-03-29 10:54:59 +0200

    Update scripts.js
    adding some comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,35 @@
 function newItem() {
 
-    //Checking if value of input is empty
+    //checks whether the value of input is empty ot not
     if ($('#input').val() === '') {
         alert('Please type something');
         return;
     }
 
-    //Get the element and the value of the input
+    //receives the element and the value of the input
     let input = $('#input').val();
     let element = $('<li></li>');
 
     //Add the element to the list
     element.append(input);
 
-    //Add the item to the list as a list element
+    //Adds the item to the list as a list element
     $('#list').append(element);
 
-    //Reset value of the input form
+    //Resets value of the input form
     $('#input').val('');
 
     //Making double click toggle the strike class
     element.on("dblclick", crossOut);
 
-    //Create delete button
+    //Creates delete button
     let deleteButton = $('<crossoutbutton></crossoutbutton>');
     deleteButton.append(document.createTextNode('Remove'));
 
-    //Append the button to the item
+    //Appends the button to the item
     element.append(deleteButton);
 
-    //Adding the listener for the click event
+    //Adds the listener for the click event
     deleteButton.on("click", deleteListItem);
 
     function deleteListItem() {
@@ -41,6 +41,6 @@ function newItem() {
         element.toggleClass('strike');
     }
 
-    //Making the list sortable
+    //Makes the list sortable
     $('#list').sortable();
 }
